daf465872070ffa9ca489d9d5b775254dba727dd
nikhil pandit <nikhilpandit264@gmail.com>
2025-09-01 09:35:31 +0530

    renderIssue

--- controllers/listing.js
+++ controllers/listing.js
@@ -27,17 +27,40 @@ module.exports.showListing =async (req, res) => {
     res.render("listings/show.ejs", { listing });
   };
   
-  module.exports.createListing =async (req, res) => {
-      let url =req.file.path;
-      let filename =req.file.filename;
+  // module.exports.createListing =async (req, res) => {
+  //     let url =req.file.path;
+  //     let filename =req.file.filename;
     
-      const newListing = new Listing(req.body.listing);
-      newListing.owner =req.user._id;
-      newListing.image = {url,filename};
-      await newListing.save();
-      req.flash("success", "New Listing Created")
-      res.redirect("/listings");
+  //     const newListing = new Listing(req.body.listing);
+  //     newListing.owner =req.user._id;
+  //     newListing.image = {url,filename};
+  //     await newListing.save();
+  //     req.flash("success", "New Listing Created")
+  //     res.redirect("/listings");
+  //   }
+
+  module.exports.createListing = async (req, res, next) => {
+  try {
+    const listing = new Listing(req.body.listing);
+
+    if (req.file) {
+      listing.image = {
+        url: req.file.path,
+        filename: req.file.filename
+      };
+    } else {
+      console.warn("⚠️ No file uploaded (req.file undefined)");
     }
+
+    listing.owner = req.user._id;
+    await listing.save();
+
+    req.flash("success", "New Listing Created!");
+    res.redirect(`/listings/${listing._id}`);
+  } catch (err) {
+    next(err); // send error to app.js logger
+  }
+};
 
     module.exports.renderEditForm = async (req, res) => {
         let { id } = req.params;
@@ -67,7 +90,7 @@ module.exports.showListing =async (req, res) => {
   module.exports.destroyListing=async (req, res) => {
       let { id } = req.params;
       let deletedListing = await Listing.findByIdAndDelete(id);
-      console.log(deletedListing);
+      // console.log(deletedListing);
           req.flash("success", " Listing Deleted")
   
       res.redirect("/listings");
